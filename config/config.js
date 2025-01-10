d1677e104684887c99661972fd5c773ea0e93b21
IzayoiS <iqbalmhasby54@gmail.com>
2025-01-10 18:25:55 +0700

    fix: deployment config

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const pg = require("pg");
 
 module.exports = {
   development: {
-    username: process.env.POSTGRES_USERNAME,
+    username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
@@ -18,7 +18,7 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.POSTGRES_USERNAME,
+    username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
